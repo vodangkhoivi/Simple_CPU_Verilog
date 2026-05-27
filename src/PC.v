b88674ff78aa389f473a22881c7a429089206ec7
Võ Đặng Khôi Vĩ <vi.vopy1592007@hcmut.edu.vn>
2026-05-27 22:14:42 +0700

    Update PC.v

--- src/PC.v
+++ src/PC.v
@@ -1,1 +1,13 @@
-
+module PC(
+    input clk,
+    input rst,
+    input ld_pc,
+    input inc_pc,
+    input [31:0] data_in,
+    output reg [31:0] pc_out
+    );
+    // 1. TODO: Implement a sequential logic block triggered by the positive edge of the clock
+    // 2. TODO: Implement synchronous active-high reset to clear the program counter
+    // 3. TODO: Implement logic to load a new address into the program counter when ld_pc is active
+    // 4. TODO: Implement logic to increment the program counter when inc_pc is active
+endmodule
